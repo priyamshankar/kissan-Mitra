86ee6910082a4ab65533b8fa01d50d8717aadd2c
priyam <priyamshankar.5@gmail.com>
2023-04-04 23:37:49 +0530

    login and signup page basic done

--- kissan-mitra-client/kissan-mitra/src/App.jsx
+++ kissan-mitra-client/kissan-mitra/src/App.jsx
@@ -1,5 +1,6 @@
 import {  BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={"this is the dashboard page"} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/signup" element={<Signup/>} />
         </Routes>
       </BrowserRouter>
     </>
