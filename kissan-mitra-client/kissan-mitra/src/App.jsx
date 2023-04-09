a83988a4b90d2e986fe26b10d37958a7690fc6f5
Kumar Shanu <kumar.shanu2020@vitbhopal.ac.in>
2023-04-09 11:18:43 +0530

    Done with the zameen info and its 6 cards

--- kissan-mitra-client/kissan-mitra/src/App.jsx
+++ kissan-mitra-client/kissan-mitra/src/App.jsx
@@ -1,6 +1,7 @@
 import {  BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import ZameenInfo from "./Components/ZameenInfo/ZameenInfo";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard/>} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/zameenInfo" element={<ZameenInfo/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
